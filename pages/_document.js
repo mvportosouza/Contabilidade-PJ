773f5a84590dd63a5e7fd623313d3db4aabb4877
mvportosouza <mvportosouza@hotmail.com>
2026-08-16 13:49:42 -0300

    Add files via upload

---
+++ pages/_document.js
@@ -0,0 +1,36 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="pt-BR">
+        <Head>
+          <meta name="theme-color" content="#0F1E35" />
+          <meta name="application-name" content="Finanças MVPS" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+          <meta name="apple-mobile-web-app-title" content="Finanças MVPS" />
+          <meta name="mobile-web-app-capable" content="yes" />
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="apple-touch-icon" href="/icon-192.png" />
+          <link rel="icon" href="/icon-192.png" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                if ("serviceWorker" in navigator) {
+                  window.addEventListener("load", function () {
+                    navigator.serviceWorker.register("/sw.js").catch(function () {});
+                  });
+                }
+              `,
+            }}
+          />
+        </body>
+      </Html>
+    );
+  }
+}
