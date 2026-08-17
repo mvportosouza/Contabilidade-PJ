b2a410e68b3ca95d6eed50c74ef3ce0a045abc3d
mvportosouza <mvportosouza@hotmail.com>
2026-08-17 09:51:52 -0300

    Configure headers for service worker in next.config.js
    Added custom headers for service worker with cache control.

--- next.config.js
+++ next.config.js
@@ -1,9 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+
   // Permite base64 grandes (logo embutida)
   images: {
     unoptimized: true,
+  },
+
+  async headers() {
+    return [
+      {
+        source: '/sw.js',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-cache, no-store, must-revalidate',
+          },
+          {
+            key: 'Content-Type',
+            value:
+              'application/javascript; charset=utf-8',
+          },
+        ],
+      },
+    ]
   },
 }
 
